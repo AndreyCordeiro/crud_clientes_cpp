8889da737621f1a504bd5c8d1f5f2cd90254f4e1
Andrey <andreybr2002@hotmail.com>
2023-03-14 20:57:15 -0300

    Ajustando implementações

--- customer.cpp
+++ customer.cpp
@@ -1,97 +1,146 @@
 #include <iostream>
 #include <string>
 #include <list>
+#include <limits>
 
 using namespace std;
 
-class Customer {
-    int id = 0;
-    int age = 0;
-    string name;
-    list<Customer> customersList;
+struct Customer {
+    int id;
+    int age;
+    char name[75];
 
-public:
-    void insert_customer() {
-        cout << "=-=-=-=-=-= CADASTRO DE CLIENTE =-=-=-=-=-=" << endl;
-
-        Customer customer;
-        int aux_id = customer.id + 1;
-        string customer_age;
-
-        customer.id = aux_id;
-
-        cout << "Informe o nome: ";
-        getline(cin, customer.name);
-
-        cout << "Informe a idade: ";
-        getline(cin, customer_age);
-        customer.age = stoi(customer_age);
-
-        customersList.push_back(customer);
-    }
-
-    void delete_customer() {
-        cout << "=-=-=-=-=-= EXCLUIR CLIENTE =-=-=-=-=-=" << endl;
-
-        int id;
-
-        cout << "Informe o ID do cliente: ";
-        cin >> id;
-
-        auto it = customersList.begin();
-
-        while (it != customersList.end()) {
-            if (it->id == id) {
-                customersList.erase(it);
-                cout << "Cliente excluido com sucesso!" << endl;
-                return;
-            } else {
-                ++it;
-            }
-            cout << "Cliente não encontrado" << endl;
-        }
-    }
-
-    void update_customer() {
-        cout << "=-=-=-=-=-= MODIFICAR CLIENTE =-=-=-=-=-=" << endl;
-        int id;
-        cout << "Informe o id do cliente que será modificado: ";
-        cin >> id;
-        bool found = false;
-
-        for (auto &customer: customersList) {
-            if (customer.id == id) {
-                found = true;
-                string customer_age;
-
-                cout << "Informe o nome: " << endl;
-                getline(cin, customer.name);
-
-                cout << "Informe a idade: " << endl;
-                getline(cin, customer_age);
-                customer.age = stoi(customer_age);
-
-                cout << "Cliente atualizado!" << endl;
-            }
-        }
-
-        if (!found) {
-            cout << "Cliente não encontrado" << endl;
-        }
-    }
-
-    void list_customer() {
-        cout << "=-=-=-=-=-= LISTAR CLIENTES =-=-=-=-=-=" << endl;
-
-        if (!customersList.empty()) {
-            for (const Customer &customer: customersList) {
-                cout << "Id: " << customer.id << endl;
-                cout << "Nome: " << customer.name << endl;
-                cout << "Idade: " << customer.age << endl;
-                cout << '\n' << endl;
-            }
-        } else {
-            cout << "Nenhum cliente cadastrado." << endl;
-        }
-    }
+    Customer *holder;
 };
+
+std::list<Customer *> customers;
+
+void insert_customer() {
+    cout << "=-=-=-=-=-= CADASTRO DE CLIENTE =-=-=-=-=-=" << endl;
+
+    Customer *customer = new Customer;
+    int lastId = 0;
+    customer->id = ++lastId; //TODO corrigir a geração de ID
+
+    cout << "Informe o nome: " << endl;
+    scanf("%s", customer->name);
+
+    cout << "Informe a idade: " << endl;
+    scanf("%i", &customer->age);
+
+    customers.push_back(customer);
+}
+
+void delete_customer() {
+    cout << "=-=-=-=-=-= EXCLUIR CLIENTE =-=-=-=-=-=" << endl;
+
+    int id;
+
+    cout << "Informe o ID do cliente: " << endl;
+    cin >> id;
+
+    auto it = customers.begin();
+
+    while (it != customers.end()) {
+        if ((*it)->id == id) {
+            customers.erase(it);
+            cout << "Cliente excluido com sucesso!" << endl;
+            return;
+        } else {
+            ++it;
+        }
+        cout << "Cliente não encontrado" << endl;
+    }
+}
+
+//TODO corrigir a função de atualizar
+void update_customer() {
+    cout << "=-=-=-=-=-= MODIFICAR CLIENTE =-=-=-=-=-=" << endl;
+    int id;
+    cout << "Informe o id do cliente que será modificado: ";
+    cin >> id;
+    bool found = false;
+
+    for (const auto customer: customers) {
+        if (customer->id == id) {
+            found = true;
+
+            cout << "Informe o nome: " << endl;
+            scanf("%s", customer->name);
+
+            cout << "Informe a idade: " << endl;
+            scanf("%i", customer->age);
+
+            cout << "Cliente atualizado!" << endl;
+        }
+    }
+
+    if (!found) {
+        cout << "Cliente não encontrado" << endl;
+    }
+}
+
+void list_customer() {
+    cout << "=-=-=-=-=-= LISTAR CLIENTES =-=-=-=-=-=" << endl;
+
+    if (!customers.empty()) {
+        for (const auto customer: customers) {
+            cout << "Id: " << customer->id << endl;
+            cout << "Nome: " << customer->name << endl;
+            cout << "Idade: " << customer->age << endl;
+            cout << '\n' << endl;
+        }
+    } else {
+        cout << "Nenhum cliente cadastrado." << endl;
+    }
+}
+
+int exit_program() {
+    cout << "Fim do programa!" << endl;
+    return 0;
+}
+
+int menu() {
+    int option;
+
+    do {
+        cout << "=-=-=-=-=-= MENU =-=-=-=-=-=" << endl;
+
+        cout << "1 - Cadastrar Cliente" << endl;
+        cout << "2 - Excluir Cliente" << endl;
+        cout << "3 - Modificar Cliente" << endl;
+        cout << "4 - Listar Clientes" << endl;
+        cout << "5 - Sair" << endl;
+
+        cout << "Escolha uma opção: ";
+        std::cin >> option;
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        switch (option) {
+            case 1:
+                insert_customer();
+                break;
+            case 2:
+                delete_customer();
+                break;
+            case 3:
+                update_customer();
+                break;
+            case 4:
+                list_customer();
+                break;
+            case 5:
+                exit_program();
+                break;
+        }
+    } while (option != 5);
+
+    return 0;
+}
+
+int main() {
+    menu();
+
+    return 0;
+}
